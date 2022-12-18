3d2c7023de1f8e3e5cdef1c329340fe776b87a02
Thom-Mon <bigtommycool@gmx.de>
2022-12-18 18:09:33 +0100

    Important optimize randomlist show

--- app/src/main/java/com/example/justone/ui/card/CardFragment.kt
+++ app/src/main/java/com/example/justone/ui/card/CardFragment.kt
@@ -25,6 +25,7 @@ class CardFragment : Fragment() {
     private var cardIndex: Int = 0;
     private  lateinit var appDb : AppDatabase
     private val gson = Gson()
+    private var randomList: List<Int> = listOf(0,0)
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -85,6 +86,14 @@ class CardFragment : Fragment() {
             {
                 cardViewModel.updateCardIndexNegative()
             }
+            // current Card TODO: als FUnctuon unbedingt auslagern und optiimieren
+            val currentCardIndex = Integer.parseInt(binding.labelCurrentCard.text as String)
+
+            binding.card1Button.text = randomList[currentCardIndex*5-5].toString();
+            binding.card2Button.text = randomList[currentCardIndex*5-4].toString();
+            binding.card3Button.text = randomList[currentCardIndex*5-3].toString();
+            binding.card4Button.text = randomList[currentCardIndex*5-2].toString();
+            binding.card5Button.text = randomList[currentCardIndex*5-1].toString();
         }
 
         binding.btnArrowNext.setOnClickListener{
@@ -92,18 +101,67 @@ class CardFragment : Fragment() {
             {
                 cardViewModel.updateCardIndexPositive()
             }
+            // current Card
+            val currentCardIndex = Integer.parseInt(binding.labelCurrentCard.text as String)
 
-            // DEBUG / TESTING
-            /*val card = Card(null,"Testwort","Tagexample",4)
-
-            GlobalScope.launch(Dispatchers.IO){
-                //appDb.entryDao().insert(entry)
-                appDb.cardDao().insert(card)
-            }*/
-
+            binding.card1Button.text = randomList[currentCardIndex*5-5].toString();
+            binding.card2Button.text = randomList[currentCardIndex*5-4].toString();
+            binding.card3Button.text = randomList[currentCardIndex*5-3].toString();
+            binding.card4Button.text = randomList[currentCardIndex*5-2].toString();
+            binding.card5Button.text = randomList[currentCardIndex*5-1].toString();
         }
 
+        binding.btnShuffleAllCard.setOnClickListener {
+            shuffleAllCards()
+        }
+
+        // set the card words on start as shuffled
+        if(randomList[0] == 0)
+        {
+            binding.card1Button.text = "TUMMELUFF"
+        }
+        else
+        {
+            binding.card1Button.text = randomList[0].toString();
+            binding.card2Button.text = randomList[1].toString();
+            binding.card3Button.text = randomList[2].toString();
+            binding.card4Button.text = randomList[3].toString();
+            binding.card5Button.text = randomList[4].toString();
+        }
+
+
         return root
+    }
+
+    private fun shuffleAllCards()
+    {
+        //val rnds = (0..1500).random()
+
+        /*for (i in 1..13)
+        {
+            print("$i ")
+            Log.e("Card_Value", "$i Kartenwert:" + (0..1500).random())
+        }*/
+
+        randomList = (0..1500).shuffled().take(65)
+        for( number in randomList)
+        {
+            Log.e("Card_Value", "List: "+number.toString())
+        }
+
+        // current Card
+        val currentCardIndex = Integer.parseInt(binding.labelCurrentCard.text as String)
+
+        binding.card1Button.text = randomList[currentCardIndex*5-5].toString();
+        binding.card2Button.text = randomList[currentCardIndex*5-4].toString();
+        binding.card3Button.text = randomList[currentCardIndex*5-3].toString();
+        binding.card4Button.text = randomList[currentCardIndex*5-2].toString();
+        binding.card5Button.text = randomList[currentCardIndex*5-1].toString();
+    }
+
+    private fun shuffleSingleCard()
+    {
+
     }
 
     override fun onDestroyView() {
